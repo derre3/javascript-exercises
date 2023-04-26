4dbfb7501a656088213d392563cb67d58c75e78a
andre <123985988+derre3@users.noreply.github.com>
2023-04-26 14:12:03 -0300

    done 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  // FIRST ATTEMPT
+    // let sumNum = 0
+
+    // num.forEach(sum => {
+    //   sumNum += sum
+    // });
+
+    // return sumNum;
+
+  return num.reduce((sum, number) => sum + number, 0)
 };
 
-const multiply = function() {
+const multiply = function(num) {
+  // FIRST ATTEMPT
+    // let multNum = 1
+    // num.forEach(mult => {
+    //   multNum *= mult
+    // });
+    // return multNum;
 
+  return num.reduce((product, number) => product *= number, 1)
 };
 
-const power = function() {
-	
+const power = function(num, numIndex) {
+  // FIRST ATTEMPT
+  let pwrNum = num
+  for (let i = 1; i < numIndex; i++) {
+    pwrNum *= num  
+  }
+  return pwrNum;
+
+// bruh
+  // return Math.pow(num, numIndex);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorialNum = 1
+
+  for (let i = 1; i <= num; i++) {
+    factorialNum *= i    
+  }
+
+  return factorialNum;
 };
 
 // Do not edit below this line
