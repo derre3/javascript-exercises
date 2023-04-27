d77a024855323c926c2e04ebc889575dbae45a49
andre <123985988+derre3@users.noreply.github.com>
2023-04-27 16:58:55 -0300

    done 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS"
+    let seq = {
+        prev: 0,
+        next: 1,
+    };
+    for (; num > 1; num--) {
+        seq.temp = seq.next;
+        seq.next += seq.prev;
+        seq.prev = seq.temp;
+    }
+    return seq.next;
 };
 
 // Do not edit below this line
