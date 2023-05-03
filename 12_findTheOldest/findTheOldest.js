18f3c07be51769ff40ce7a0db6ecbe95bfd4e7be
Andre <123985988+derre3@users.noreply.github.com>
2023-05-03 13:51:38 -0300

    done 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleAges = people.map(person => {
+        person.yearOfDeath === undefined 
+            ? person.yearOfDeath = (new Date).getFullYear() 
+            : false
+        return person.yearOfDeath - person.yearOfBirth;
+    });   
+    return people[peopleAges.indexOf(Math.max(...peopleAges))]
 };
 
 // Do not edit below this line
